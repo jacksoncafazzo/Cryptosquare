59ba0566d8b33371354ca7f22d45312ae923d792
Jackson Cafazzo and Ryan Brown <doctorjuno@gmail.com>
2016-01-19 15:17:58 -0800

    Add initial files, characterLength function.

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+var characterLength = function(message) {
+  return message.length;
+
+}
+
+// $(document).ready(function() {
+//
+//
+//     event.preventDefault();
+//   });
---
+++ spec/specs.js
@@ -0,0 +1,5 @@
+describe('characterLength', function() {
+  it("has characters", function() {
+    expect(characterLength("dog")).to.equal(3);
+  });
+});
