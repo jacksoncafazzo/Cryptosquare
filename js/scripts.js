5bcae2da93b44b779985de736685e5f045fcc487
Jackson Cafazzo and Ryan Brown <browneryan87@gmail.com>
2016-01-19 16:10:34 -0800

    Add square root, ceiling methods to characterLength.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,18 @@
-
-var characterLength = function(message) {
-  return message.length;
-}
-
 var areSpaces = function(message) {
   return message.indexOf(' ') >= 0;
 }
 
 var removeSpaces = function(message) {
   return message.replace(/\s+/g, "");
-  }
+}
+
+var characterLength = function(removeSpaces) {
+  return removeSpaces.length;
+}
+
+var columnLength = function(characterLength) {
+  return Math.ceil(Math.sqrt(characterLength));
+}
 
 
 // $(document).ready(function() {
--- spec/specs.js
+++ spec/specs.js
@@ -1,9 +1,3 @@
-describe('characterLength', function() {
-  it("has characters", function() {
-    expect(characterLength("dog")).to.equal(3);
-  });
-});
-
 describe('areSpaces', function() {
   it("has spaces", function() {
     expect(areSpaces("any ol message")).to.equal(true);
@@ -13,5 +7,17 @@ describe('areSpaces', function() {
 describe('removeSpaces', function() {
   it("removes spaces", function() {
     expect(removeSpaces("any ol message")).to.equal("anyolmessage");
+  });
+});
+
+describe('characterLength', function() {
+  it("has characters", function() {
+    expect(characterLength("dog")).to.equal(3);
+  });
+});
+
+describe('columnLength', function() {
+  it("has characters", function() {
+    expect(columnLength(13)).to.equal(4);
   });
 });
